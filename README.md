35f3be9f4a284007f6beb460d6360ce27fe2230d
Grace Buttler <grace.buttler@gmail.com>
2025-03-20 17:42:36 +0000

    begin working on the gameboard class

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ### Aims
 
 - To implement the game 'Battleships' using TDD (with Jest)
+- To use classes instead of factory functions (simply because I haven't used classes as much and want to practise!)
+- Make my code and simple and 'pure' as possible
--- script.js
+++ script.js
@@ -17,4 +17,23 @@ class Ship {
   }
 }
 
-export { Ship };
+class Gameboard {
+  constructor() {
+    this.board = [];
+  }
+
+  //create gameboard 10x10 squares
+  createBoard() {
+    for (let i = 0; i < 100; i++) {
+      this.board.push("");
+    }
+    return this.board;
+  }
+
+  //split up the gameboard into rows (like tic tac toe)
+  //allow a ship of a specific length to be placed on the board (and not allowed if it won't fit) - specific coordinates
+  //recieveAttack function (ship hit or not, ship sunk or not.) Mark space accordingly
+  //check if all ships are sunk
+}
+
+export { Ship, Gameboard };
